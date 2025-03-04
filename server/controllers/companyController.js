350a4ee6d640e75459871adbbf107c1c5e08bb0f
sonikakannan <sonikakannan66@gmail.com>
2025-03-04 12:42:49 +0530

    first commit

---
+++ server/controllers/companyController.js
@@ -0,0 +1,14 @@
+// Register a new company 
+export const registerCompany =async(req,res)=>{
+
+}
+
+// Company login 
+export const loginCompany =async(req,res)=>{
+
+}
+
+//Get Company data
+export const getCompanyData =async(req,res)=>{
+
+}
